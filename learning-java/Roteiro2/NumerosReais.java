e623358666706f0fd302f1e7c7fadf0ddd181666
Mateus Filipe <mateusfilipe557@gmail.com>
2019-08-08 10:48:10 -0300

    Update NumerosReais.java

--- learning-java/Roteiro2/NumerosReais.java
+++ learning-java/Roteiro2/NumerosReais.java
@@ -4,13 +4,13 @@ import java.util.*;
 public class NumerosReais {
     public static void main(String[] args){
         Scanner S1 =  new Scanner(System.in);
-        double n1 = S1.nextInt();
+        double n1 = S1.nextDouble();
 
         Scanner S2 =  new Scanner(System.in);
-        double n2 = S2.nextInt();
+        double n2 = S2.nextDouble();
 
         Scanner S3 =  new Scanner(System.in);
-        double n3 = S3.nextInt();
+        double n3 = S3.nextDouble();
         double resultado = (n1*n2)/n3;
         System.out.println("Média: "+resultado);
     }
