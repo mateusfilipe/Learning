4e40a4586e4887a06b6ff5c2ded4d42b26a1c9d1
Mateus Filipe <mateusfilipe557@gmail.com>
2020-10-11 22:05:22 -0300

    Update Program.cs

--- learning-cSharp/leitura-de-arquivo/leitura-de-arquivo/Program.cs
+++ learning-cSharp/leitura-de-arquivo/leitura-de-arquivo/Program.cs
@@ -7,11 +7,13 @@ namespace leitura_de_arquivo
         static void Main(string[] args)
         {
             // Método 1, fazendo a leitura e salvando numa string
-            string text = System.IO.File.ReadAllText(@"C:\Users\T-Gamer\source\repos\leitura-de-arquivo\leitura-de-arquivo\teste.txt");
+                                                    //Repositório completo do arquivo
+            string text = System.IO.File.ReadAllText(@"\teste.txt");
             Console.WriteLine("Conteúdo do arquivo: {0}", text);
 
             //Método 2, fazendo a leitra e salvando em um vetor de string
-            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\T-Gamer\source\repos\leitura-de-arquivo\leitura-de-arquivo\teste.txt");
+                                                    //Repositório completo do arquivo
+            string[] lines = System.IO.File.ReadAllLines(@"\teste.txt");
             //Impirmindo o vetor de strings por um foreach
             foreach (string line in lines) {
                 Console.WriteLine("\t" + line);
