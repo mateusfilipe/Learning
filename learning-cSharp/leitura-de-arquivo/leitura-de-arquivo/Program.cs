e37341d69f1e529ac768f5fc08553fc23e988f12
Mateus Filipe <mateusfilipe557@gmail.com>
2020-10-11 21:36:14 -0300

    Folder update and adding C# code

---
+++ learning-cSharp/leitura-de-arquivo/leitura-de-arquivo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace leitura_de_arquivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Método 1, fazendo a leitura e salvando numa string
+            string text = System.IO.File.ReadAllText(@"C:\Users\T-Gamer\source\repos\leitura-de-arquivo\leitura-de-arquivo\teste.txt");
+            Console.WriteLine("Conteúdo do arquivo: {0}", text);
+
+            //Método 2, fazendo a leitra e salvando em um vetor de string
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\T-Gamer\source\repos\leitura-de-arquivo\leitura-de-arquivo\teste.txt");
+            //Impirmindo o vetor de strings por um foreach
+            foreach (string line in lines) {
+                Console.WriteLine("\t" + line);
+            }
+        }
+    }
+}
